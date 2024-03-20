67c782083aaaaf84e92342e478ea41b1e378346f
Andrew Miller <socrates1024@gmail.com>
2024-03-19 22:28:10 -0500

    fix script

--- sortedlists/run_benchmark.sh
+++ sortedlists/run_benchmark.sh
@@ -5,7 +5,7 @@ rm -r build_circuits
 mkdir -p build_circuits
 
 # Loop over sizes
-for i in $(seq 4 10)
+for i in $(seq 4 12)
 do
 
     # Generate a new build/2^{i} folder
@@ -13,11 +13,11 @@ do
     pushd build_circuits/2^${i}
 
     # replace {{N}} in src/main.nr    
-    cat ../../src/main.nr | CIRCUIT_SIZE=$(($i**2)) envsubst > ./src/main.nr
+    cat ../../src/main.nr | CIRCUIT_SIZE=$((2**$i)) envsubst > ./src/main.nr
     
     # Generate the Prover.toml with an array
     echo >> ./Prover.toml "x = ["    
-    for j in $(seq 1 $(($i**2)))
+    for j in $(seq 1 $((2**i)))
     do
 	cat >> ./Prover.toml <<EOF
 	${j},
@@ -29,7 +29,7 @@ EOF
     nargo compile
     nargo info | tee ../info_${i}.txt
 
-    time nargo prove > ../time_proof_${i}.txt
+    { time nargo prove; } 2> ../time_proof_${i}.txt
 
     popd
 done
